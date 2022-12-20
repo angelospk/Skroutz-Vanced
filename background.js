b423a578847f0bca9902d3c1ae0b6967f58136f1
angelospk <rovas450@gmail.com>
2022-12-20 19:27:31 +0200

    0.0.1

--- background.js
+++ background.js
@@ -1,3 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('loaded');
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' ) {
+    if (tab.url.includes("/s/")){
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        files: ["./product.js"]
+    })
+        .then(() => {
+            console.log("etrekse to product script.");
+        })
+        .catch(err => console.log(err));
+    }
+    else if (tab.url.includes("/c/") || tab.url.includes("gr/search?")){
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        files: ["./search.js"]
+    })
+        .then(() => {
+            console.log("etrkse to search script.");
+        })
+        .catch(err => console.log(err));
+    }
+    else if (tab.url.includes("skroutz.gr/cart")){
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        files: ["./cart.js"]
+    })
+        .then(() => {
+            console.log("etrkse to cart script.");
+        })
+        .catch(err => console.log(err));
+    }
+      
+  }
 });
